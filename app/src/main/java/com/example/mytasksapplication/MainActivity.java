038dc80d49956d34f54af753a8e967a6b318efe1
mujumdarsmita <smitamujumdar94@gmail.com>
2022-01-19 16:08:50 +0530

    Initial commit - project setup and login screen.

---
+++ app/src/main/java/com/example/mytasksapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mytasksapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+  BottomNavigationView bottomNavigationView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+  }
+}
